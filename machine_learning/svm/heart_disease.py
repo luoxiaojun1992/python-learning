16b0c5b6439b6a685697405badcf1f99e53066d1
罗晓俊 <luoxiaojun1992@sina.cn>
2017-03-14 18:40:54 +0800

    modify heart disease

--- machine_learning/svm/heart_disease.py
+++ machine_learning/svm/heart_disease.py
@@ -8,13 +8,14 @@ import sklearn.preprocessing
 iris = np.loadtxt('./heart-disease.data', delimiter=',', dtype='str')
 x, y = iris[:, :13], iris[:, 13].astype(np.int)
 
+le = sklearn.preprocessing.LabelEncoder()
+
 x_data = []
 for item in x:
-    
+   le.fit(item)
+   new_item = le.transform(item)
+   x_data.append(new_item)
 
 svm = mlpy.LibSvm()
 svm.learn(np.array(x_data), np.array(y))
-print svm.pred(x_data[0])
-print svm.pred(x_data[len(x_data) - 1])
 print svm.pred(x_data[len(x_data) - 3])
-print svm.pred(x_data[len(x_data) - 10])
