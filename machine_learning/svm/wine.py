401c2a28570414e9b2f4ac04d865bd079c57a54d
罗晓俊 <luoxiaojun1992@sina.cn>
2017-03-15 14:44:09 +0800

    modify wine

--- machine_learning/svm/wine.py
+++ machine_learning/svm/wine.py
@@ -3,20 +3,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mlpy
-import sklearn.preprocessing
 
-iris = np.loadtxt('./wine.data', delimiter=',')
-x, y = iris[:, :13], iris[:, 13].astype(np.int)
-
-#x_data = []
-#for item in x:
-#    x_data.append(sklearn.preprocessing.normalize(item, norm='l2'))
-
-#y = sklearn.preprocessing.normalize(y, norm='l2')
-
-x_data = x
+wine = np.loadtxt('./wine.data', delimiter=',')
+x, y = wine[:, :13], wine[:, 13]
 
 svm = mlpy.LibSvm()
-svm.learn(np.array(x_data), np.array(y))
-print svm.pred(x_data[0])
-print svm.pred(x_data[len(x_data) - 1])
+svm.learn(np.array(x), np.array(y))
+print svm.pred(x[0])
+print svm.pred(x[len(x) - 1])
